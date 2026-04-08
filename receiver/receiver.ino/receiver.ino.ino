f014c3156ad3bf2b321dd08cb9b4a30c78d5f7ba
Jetorific <jtrom@illinois.edu>
2026-04-07 19:33:49 -0500

    updated WIP code

--- receiver/receiver.ino/receiver.ino.ino
+++ receiver/receiver.ino/receiver.ino.ino
@@ -1,56 +1,110 @@
 /*
-  Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  Modified for SBUS Betaflight bridging
 */
 
 #include <esp_now.h>
 #include <WiFi.h>
+#include "sbus.h"
+
+// Note: Replaced the magic numbers 33/32 with your defined pins
+#define RXD1 18
+#define TXD1 17
+
+// Initialize bolderflight SBUS TX on Serial1
+// Note: Some older versions of this library use the rx/tx/invert bools in the constructor. 
+// If true/false throws an error, update to just: bfs::SbusTx sbus_tx(&Serial1);
+bfs::SbusTx sbus_tx(&Serial1, RXD1, TXD1, true, false);
+bfs::SbusData data;
 
 // Structure example to receive data
-// Must match the sender structure
 typedef struct data_struct_t {
-    float x;
-    float y;
-    float z;
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
+  int button;
 } data_struct;
 
-// Create a struct_message called myData
 data_struct myData;
+
+// Timer for continuous SBUS transmission
+unsigned long lastSbusTime = 0;
+
+void PrintMPUData() {
+  Serial.print("ax: "); Serial.println(myData.ax);
+  Serial.print("ay: "); Serial.println(myData.ay);
+  Serial.print("az: "); Serial.println(myData.az);
+  Serial.println("");
+}
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.println("xyz received:");
-  Serial.println(myData.x);
-  Serial.println(myData.y);
-  Serial.println(myData.z);
-  Serial.println("");
+  // PrintMPUData(); // Uncomment for debugging, but keeping prints out of callbacks is safer
 }
- 
-void setup() {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
+// Helper function to map float IMU values (-1.0 to 1.0) to SBUS integer format (192 to 1792)
+int mapFloatToSBUS(float val, float min_in, float max_in) {
+  // Constrain to prevent SBUS channel overflow
+  if (val < min_in) val = min_in;
+  if (val > max_in) val = max_in;
+  
+  // Map range: 192 (1000us) to 1792 (2000us)
+  float mapped = (val - min_in) * (1792.0 - 192.0) / (max_in - min_in) + 192.0;
+  return (int)mapped;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize SBUS Transmission
+  sbus_tx.Begin();
+
+  // Set default safe SBUS values (Centered sticks, zero throttle)
+  for (int i = 0; i < 16; i++) {
+    data.ch[i] = 992; // 1500us center
+  }
+  data.ch[2] = 192;   // Throttle (CH3) to zero (1000us)
+
+  WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
   esp_now_register_recv_cb((esp_now_recv_cb_t)OnDataRecv);
 }
  
 void loop() {
+  // SBUS requires a continuous stream of data (~100Hz) to prevent RX_FAILSAFE
+  if (millis() - lastSbusTime >= 10) {
+    lastSbusTime = millis();
 
+    // Map axes assuming -1.0g to 1.0g scale. Adjust the min/max limits 
+    // depending on the actual raw output scale of your specific IMU.
+    
+    // CH1 (Roll): Maps horizontal Y acceleration
+    data.ch[0] = mapFloatToSBUS(myData.ay, -1.0, 1.0); 
+    
+    // CH2 (Pitch): Maps horizontal X acceleration
+    data.ch[1] = mapFloatToSBUS(myData.ax, -1.0, 1.0);
+    
+    // CH3 (Throttle): Maps vertical Z acceleration. 
+    // DANGER: If your IMU reads 1.0g at rest due to gravity, this will send 
+    // full throttle! You must subtract gravity or map this to 0.0 - 1.0.
+    data.ch[2] = mapFloatToSBUS(myData.az, 0.0, 1.0);  
+    
+    // CH4 (Yaw): Kept centered so the drone doesn't spin
+    data.ch[3] = 992;
+    
+    // CH5 (AUX1): Use the button to act as an Arm/Disarm switch
+    // Button pressed (1) sends 2000us, released (0) sends 1000us
+    data.ch[4] = myData.button ? 1792 : 192;
+
+    // Send the packet to the flight controller
+    sbus_tx.data(data);
+    sbus_tx.Write();
+  }
 }
--- sender/sender.ino/sender.ino.ino
+++ sender/sender.ino/sender.ino.ino
@@ -11,6 +11,11 @@
 
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
 
 #define LED_PIN 2
 #define BUTTON_PIN 15
@@ -23,9 +28,13 @@ uint8_t broadcastAddress[] = {0xF0, 0x08, 0xD1, 0x49, 0xD4, 0xAC};
 // Structure example to send data
 // Must match the receiver structure
 typedef struct data_struct_t {
-  float x;
-  float y;
-  float z;
+  float ax;
+  float ay;
+  float az;
+  float gx;
+  float gy;
+  float gz;
+  int button;
 } data_struct;
 
 // Create a struct_message called myData
@@ -50,14 +59,106 @@ void SendData(data_struct *myData) {
     Serial.println("Error sending the data");
   }
 }
- 
+
+void InitializeMPU() {
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+}
+
+void PrintMPUData() {
+  Serial.print("ax: ");
+  Serial.println(myData.ax);
+  Serial.print("ay: ");
+  Serial.println(myData.ay);
+  Serial.print("az: ");
+  Serial.println(myData.az);
+  Serial.print("gx: ");
+  Serial.println(myData.gx);
+  Serial.print("gy: ");
+  Serial.println(myData.gy);
+  Serial.print("gz: ");
+  Serial.println(myData.gz);
+  Serial.println("");
+}
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
 
   // Init Serial Monitor
   Serial.begin(115200);
- 
+
+  InitializeMPU();
+
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -87,14 +188,23 @@ void loop() {
   int button_value = digitalRead(BUTTON_PIN);
   digitalWrite(LED_PIN, button_value);
 
-  myData.x = 1;
-  myData.y = 1;
-  myData.z = 1;
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  myData.ax = a.acceleration.x;
+  myData.ay = a.acceleration.y;
+  myData.az = a.acceleration.z;
+  myData.gx = g.gyro.x;
+  myData.gy = g.gyro.y;
+  myData.gz = g.gyro.z;
+  myData.button = button_value;
 
   if (button_value == 1 && previous_button_value == 0) {
     Serial.println("Button pressed! Sending data.");
     SendData(&myData);
   }
+
+  PrintMPUData();
 
   previous_button_value = button_value;
 }
